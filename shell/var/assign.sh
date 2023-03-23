de6e0139991eff9e702bca24f2ac4012f7f0f1cc
Williams <wkindman4@gmail>
2023-03-23 23:05:10 +0100

    My first commit here

--- shell/var/assign.sh
+++ shell/var/assign.sh
@@ -6,5 +6,17 @@ read name
 
 echo "Welcome to group_5 $name"
 
+read -p "Enter your score: " score
+
+if ((score < 80));
+then
+	echo "Sory, you're out, do try again later"
+elif ((score == 80));
+then
+	echo "You where lucky! Congrats"
+elif ((score > 80));
+then
+	echo "You're so good! Welcome"
+fi
 
 
